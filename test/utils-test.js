2ead6244dc42048df5b006eeffcc29fc9233d1ac
john andrey <andrey_201015@hotmail.com>
2017-08-28 23:08:55 -0500

    Pruebas unitarias con arrays

--- test/utils-test.js
+++ test/utils-test.js
@@ -1,19 +1,48 @@
 'use strict'
 
 const test = require('ava')
-// un test que solo pasa sin validar nada 
+const utils = require('../lib/utils')
+
+/*
+// un test que solo pasa sin validar nada
 test('this, should pass', t => {
   t.pass()
 })
 
 // Un test que da error sin validar
-/* test('this should fail', t => {
+test('this should fail', t => {
   t.fail()
-}) */
+})
 
-// un test que siendo async 
+// un test que siendo async
 test('it should support async/await', async t => {
   let p = Promise.resolve(43)
   let secret = await p
   t.is(secret, 43)
+})
+
+*/
+
+// Estamos definiendo lo que yo quiero que me devuelva
+test('extracting hashtags from text', t => {
+  let tags = utils.extractTags('a #picture with tags #AwEsOmE #Platzi #AVA #100 ##yes')
+  // deepEqual sirve para comparar objects o arrays
+  t.deepEqual(tags, [
+    'picture',
+    'awesome',
+    'platzi',
+    'ava',
+    '100',
+    'yes'
+  ])
+
+  // Si el texto no tiene #
+  tags = utils.extractTags('a picture with no tags')
+  t.deepEqual(tags, [])
+  // si no pasan nada ningun text
+  tags = utils.extractTags()
+  t.deepEqual(tags, [])
+
+  tags = utils.extractTags(null)
+  t.deepEqual(tags, [])
 })
