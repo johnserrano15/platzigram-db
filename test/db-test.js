315fc681e8c1fef41c794e1525169b75a3ae2ad0
john andrey <andrey_201015@hotmail.com>
2017-09-11 23:12:39 -0500

    terminando implementacion de test y creacion de images, y test de liked a mis images

--- test/db-test.js
+++ test/db-test.js
@@ -4,6 +4,7 @@ const test = require('ava')
 const uuid = require('uuid-base62')
 const r = require('rethinkdb')
 const Db = require('../')
+const fixtures = require('./fixtures')
 
 const dbName = `platzigram_${uuid.v4()}`
 const db = new Db({ db: dbName })
@@ -26,18 +27,34 @@ test.after.always('cleanup database', async t => {
 test('save image', async t => {
   t.is(typeof db.saveImage, 'function', 'Save Image in function ')
 
-  let image = {
-    url: `https://platzigram.test/${uuid.v4()}.jpg`,
-    likes: 0,
-    liked: false,
-    user_id: uuid.uuid()
-  }
+  let image = fixtures.getImage()
+
   let created = await db.saveImage(image)
+  // validamos que lo se esta pasando es lo mismo que ya se creo.
+  t.is(created.description, image.description)
   t.is(created.url, image.url)
   t.is(created.likes, image.likes)
   t.is(created.liked, image.liked)
+  t.deepEqual(created.tags, ['awesome', 'platzi'])
   t.is(created.user_id, image.user_id)
   t.is(typeof created.id, 'string')
+  t.is(created.public_id, uuid.encode(created.id))
   // Fecha de creacion
   t.truthy(created.createdAt)
+})
+
+test('like image', async t => {
+  // preguntamos que exista una funcion con nmae likeImage
+  t.is(typeof db.likeImage, 'function', 'like images is a function')
+  // Traemos la imagen
+  let image = fixtures.getImage()
+  // guardamos la imagen
+  let created = await db.saveImage(image)
+  // le pasamos el id public a la function likeImage
+  let result = await db.likeImage(created.public_id)
+
+  // validamos que el resultado si sea like
+  t.true(result.liked)
+  // Y que el liked aya aumentado a mas uno
+  t.is(result.likes, image.likes + 1)
 })
